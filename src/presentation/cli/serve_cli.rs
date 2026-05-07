bda2063fce8f216e72cc7bedeac118a0f1d63056
Kosuke Mori <kosuke.mori.5120@gmail.com>
2026-05-08 07:18:34 +0900

    add health handler

--- src/presentation/cli/serve_cli.rs
+++ src/presentation/cli/serve_cli.rs
@@ -1,5 +1,12 @@
+use crate::presentation::handler::health_handler::health_handler;
+use axum::{Router, routing::get};
 use std::net::SocketAddr;
 
-pub async fn run(_addr: SocketAddr) -> Result<(), std::io::Error> {
-    todo!()
+pub async fn run(addr: SocketAddr) -> Result<(), std::io::Error> {
+    let api_routes = Router::new().route("/health", get(health_handler));
+
+    let app = Router::new().nest("/v1", api_routes);
+
+    let listener = tokio::net::TcpListener::bind(addr).await?;
+    axum::serve(listener, app).await
 }
